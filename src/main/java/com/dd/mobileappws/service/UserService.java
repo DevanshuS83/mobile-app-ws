4768e14e63f1bc1b93f7ed9e9afe7f0c82a5a4b3
Devanshu <devanshusachdev83@gmail.com>
2021-11-14 16:03:26 +0530

    Configured Spring Security for user sign-up

--- src/main/java/com/dd/mobileappws/service/UserService.java
+++ src/main/java/com/dd/mobileappws/service/UserService.java
@@ -1,8 +1,10 @@
 package com.dd.mobileappws.service;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 import com.dd.mobileappws.shared.dto.UserDto;
 
-public interface UserService {
+public interface UserService extends UserDetailsService{
 
 	public UserDto createUser(UserDto user);
 }
